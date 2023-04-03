dc594a5fafd6b5c2e0a1c2340f604e7e39a1d22c
vkolek <vkolek@udesa.edu.ar>
2023-04-03 15:43:44 -0300

    Valen

---
+++ db/info.js
@@ -0,0 +1,41 @@
+info = {
+    products: [
+        {
+            nombre: 'Shampoo Kerastase',
+            cover: '/public/imahes/products/shampookerastase.png',
+            descripcion: 'Shampoo Kerastase super hidratante',
+            fechacarga: '05/02/2023',
+            id: 1,
+        },
+        {
+            nombre: 'Labial Dior',
+            cover: '/public/imahes/products/labialdior.webp',
+            descripcion: 'Labial Dior rojo humectante',
+            fechacarga: '07/02/2023',
+            id: 2,
+        },
+        {
+            nombre: 'Mascara Lancome',
+            cover: '/public/imahes/products/mascaralancome.webp',
+            descripcion: 'Máscara Lancome grandiosa',
+            fechacarga: '09/02/2023',
+            id: 3,
+        },
+        {
+            nombre: 'Acondicionador Kerastase',
+            cover: '/public/imahes/products/acondicionadorker.webp',
+            descripcion: 'Acondicionador Kerastase super hidratante',
+            fechacarga: '11/03/2023',
+            id: 4,
+        },
+        {
+            nombre: 'Desmaquillante Lancome',
+            cover: '/public/imahes/products/desmaquillante.webp',
+            descripcion: 'Desmaquillante Lancome super suave',
+            fechacarga: '13/03/2023',
+            id: 5,
+        }
+    ]
+}
+
+module.exports = info;
