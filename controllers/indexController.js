e3d9e29fbc7be2e995263010f02987f47021a231
Ema Gondell <emagondell@Emas-Air.fibertel.com.ar>
2023-04-03 14:55:49 -0300

    ema valn juli index controler

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,19 @@
 const indexController = {
-    index : function(req, res, next) {
+    index : function(req, res) {
         res.render('index', { title: 'Express' });
-      }
+      },
+      login: function (req, res){
+        res.render('login')
+      },
+      register: function (req, res){
+        res.render('register')
+      },
+      results: function (req, res){
+        res.render('search-results')
+      },
+
+
+
 }
+
+module.exports = indexController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
+router.get('/login', indexController.login);
+router.get('/register', indexController.register);
+router.get('/results', indexController.results);
+
+
 
 module.exports = router;
