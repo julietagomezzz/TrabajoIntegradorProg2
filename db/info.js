ef8298cc28a5302112aaab7ef5eac462fe03a772
vkolek <vkolek@udesa.edu.ar>
2023-04-03 16:13:18 -0300

    terminé info js

--- db/info.js
+++ db/info.js
@@ -2,40 +2,64 @@ info = {
     products: [
         {
             nombre: 'Shampoo Kerastase',
-            cover: '/public/imahes/products/shampookerastase.png',
+            cover: '/public/images/products/shampookerastase.png',
             descripcion: 'Shampoo Kerastase super hidratante',
             fechacarga: '05/02/2023',
             id: 1,
         },
         {
             nombre: 'Labial Dior',
-            cover: '/public/imahes/products/labialdior.webp',
+            cover: '/public/images/products/labialdior.webp',
             descripcion: 'Labial Dior rojo humectante',
             fechacarga: '07/02/2023',
             id: 2,
         },
         {
             nombre: 'Mascara Lancome',
-            cover: '/public/imahes/products/mascaralancome.webp',
+            cover: '/public/images/products/mascaralancome.webp',
             descripcion: 'Máscara Lancome grandiosa',
             fechacarga: '09/02/2023',
             id: 3,
         },
         {
             nombre: 'Acondicionador Kerastase',
-            cover: '/public/imahes/products/acondicionadorker.webp',
+            cover: '/public/images/products/acondicionadorker.webp',
             descripcion: 'Acondicionador Kerastase super hidratante',
             fechacarga: '11/03/2023',
             id: 4,
         },
         {
             nombre: 'Desmaquillante Lancome',
-            cover: '/public/imahes/products/desmaquillante.webp',
+            cover: '/public/images/products/desmaquillante.webp',
             descripcion: 'Desmaquillante Lancome super suave',
             fechacarga: '13/03/2023',
             id: 5,
         }
-    ]
+    ],
+    coments: [
+        {
+            nombresuario:'emagondell',
+            texto: '¿Tienen stock disponible?',
+            cover: '/public/images/users/ema.jpeg'
+        },
+        {
+            nombresuario:'julietagomez',
+            texto: '¿Tienen envíos a todo el país?',
+            cover: '/public/images/users/juli.png'
+        },
+        {
+            nombresuario:'valenkolek',
+            texto: '¿Tienen envío gratis?',
+            cover: '/public/images/users/valen.png'
+        }
+    ],
+    user: {
+        email: 'julivalenema@gmail.com',
+        usuario: 'julivalenema',
+        contraseña: 'trabajointegrador',
+        dni: '28630542',
+        foto: '/public/images/users/fotoperfil.avif'
+    }
 }
 
 module.exports = info;
