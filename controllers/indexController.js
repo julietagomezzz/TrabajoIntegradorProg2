868f51436ae8c0f5adf94126f76df283cf81cb21
vkolek <vkolek@udesa.edu.ar>
2023-04-04 09:33:01 -0300

    arregle la carpeta data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,17 @@
-const products = require('../db/info')
+const products = require('../data/info')
 
 const indexController = {
     index : function(req, res) {
-        return res.render('index', { products: info.products });
+          return res.render('index', { products: info.products });
       },
       login: function (req, res){
-        return res.render('login')
+          return res.render('login')
       },
       register: function (req, res){
-        return res.render('register')
+          return res.render('register')
       },
       results: function (req, res){
-        return res.render('search-results')
+          return res.render('search-results')
       },
 
 
