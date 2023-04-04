92004c7293055386f8a49a91b8b33795990dea31
Ema Gondell <emagondell@Emas-Air.fibertel.com.ar>
2023-04-04 19:12:24 -0300

    pruebas css

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,11 @@
 const productsController ={
-    index: function(req, res,) {
+    
+  index: function(req, res,) {
       },
-    detalle: function(req,res){
-      id = req.params.id
-      for(let i = 0; i< info.products.lenght; i++){
+  
+  detalle: function(req,res){
+    id = req.params.id
+    for(let i = 0; i< info.products.lenght; i++){
         if( id== info.products[i].id){
             product_info = info.products[i]
         }
@@ -11,6 +13,7 @@ const productsController ={
       res.render('product', {id:id, products: info.products, product_info: product_info, comments:info.comments})
 
     },
+  
     add: function (req,res) {
       res.render('product-add', {user:info.user})
     }
