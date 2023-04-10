63294c3567a45454741316b521ce76c76777adfc
Ema Gondell <emagondell@Emas-MacBook-Air.local>
2023-04-10 08:23:33 -0300

    arreglo headerlogueado y sql

--- sql/proyecto.sql
+++ sql/proyecto.sql
@@ -1,4 +1,5 @@
 create schema trabajoIntegradorProg2;
+
 use trabajoIntegradorProg2;
 
 create table usuarios(
@@ -8,33 +9,33 @@ contrasena varchar(250) not null,
 fotoDePerfil varchar (200),
 fecha date,
 dni int unsigned,
-createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+createdAt timestamp default current_timestamp,
 updatedAt timestamp default current_timestamp on update current_timestamp,
 deletedAt timestamp default current_timestamp
 );
 
-use trabajoIntegradorProg2;
 
 create table productos(
 id int unsigned primary key auto_increment,
 nombre varchar(250) not null,
 descripcion varchar(500) not null,
-usuario_id int,
-foreign key (usuario_id) references ususarios(id),
+usuario_id int unsigned,
 createdAt timestamp default current_timestamp,
 updatedAt timestamp default current_timestamp on update current_timestamp,
-deletedAt timestamp default current_timestamp
+deletedAt timestamp default current_timestamp,
+foreign key (usuario_id) references ususarios(id)
 );
 
-use trabajoIntegradorProg2;
 
 create table comentarios(
-id_post int unsigned auto_increment,
+id_post int unsigned primary key auto_increment,
 id_usuario int,
 comentarios text,
-usuario_id int,
-foreign key (usuario_id) references usuario(id),
+usuario_id int unsigned,
+producto_id int unsigned,
 createdAt timestamp default current_timestamp,
 updatedAt timestamp default current_timestamp on update current_timestamp,
-deletedAt timestamp default current_timestamp
+deletedAt timestamp default current_timestamp,
+foreign key (usuario_id) references usuario(id),
+foreign key (producto_id) references producto(id)
 );
