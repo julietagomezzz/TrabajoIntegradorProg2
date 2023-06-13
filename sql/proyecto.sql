a92e66fde5d17e1d1f1cb6e3960065fd8dbde798
Ema Gondell <egondell@udesa.edu.ar>
2023-06-13 10:00:08 -0300

    nueva base de datos

--- sql/proyecto.sql
+++ sql/proyecto.sql
@@ -4,6 +4,7 @@ use trabajoIntegradorProg2;
 
 create table usuarios(
 id int unsigned primary key auto_increment,
+nombre varchar (200) not null,
 email varchar(100) unique not null,
 contrasena varchar(250) not null,
 fotoDePerfil varchar (200),
@@ -11,13 +12,13 @@ fecha date,
 dni int unsigned,
 createdAt timestamp default current_timestamp,
 updatedAt timestamp default current_timestamp on update current_timestamp,
-deletedAt timestamp default current_timestamp,
-nombre varchar (200) not null
+deletedAt timestamp default current_timestamp
 );
 
 
 create table productos(
 id int unsigned primary key auto_increment,
+cover text,
 nombre varchar(250) not null,
 descripcion varchar(500) not null,
 usuarioId int unsigned not null,
@@ -39,27 +40,27 @@ foreign key (usuarioId) references usuarios(id),
 foreign key (productoId) references productos(id)
 );
 
-insert into usuarios (id, email, contrasena, fotoDePerfil, fecha, dni)
-values (default, 'ema@udesa.com', 'contrasena1', '/images/users/ema.jpeg', '2023-1-1', 11111),
-(default, 'julietagomez@udesa.com', 'contrasena2', '/images/users/juli.png', '2023-1-1', 22222),
-(default, 'valenkolek@udesa.com', 'contrasena3', '/images/users/valen.png', '2023-1-1', 33333),
-(default, 'juanneira@udesa.com','contrasena4', '/images/users/juan.jpeg', '2023-1-1', 44444 ),
-(default, 'pilarmedico@udesa.com','contrasena5', '/images/users/pili.jpeg', '2023-1-1', 55555 );
+insert into usuarios (id,nombre, email, contrasena, fotoDePerfil, fecha, dni)
+values (default,'Ema', 'ema@udesa.com', 'contrasena1', '/images/users/ema.jpeg', '2023-1-1', 11111),
+(default, 'Julieta', 'julietagomez@udesa.com', 'contrasena2', '/images/users/juli.png', '2023-1-1', 22222),
+(default, 'valentina','valenkolek@udesa.com', 'contrasena3', '/images/users/valen.png', '2023-1-1', 33333),
+(default, 'Juan','juanneira@udesa.com','contrasena4', '/images/users/juan.jpeg', '2023-1-1', 44444 ),
+(default, 'Pili', 'pilarmedico@udesa.com','contrasena5', '/images/users/pili.jpeg', '2023-1-1', 55555 );
 
 
 
 
-insert into productos (id, nombre, descripcion, usuarioId)
-values (default, 'Shampoo Kerastase', 'Shampoo Kerastase super hidratante', 1),
-(default, 'Labial Dior', 'Labial Dior rojo humectante', 2),
-(default, 'Mascara Lancome', 'Máscara Lancome grandiosa', 3),
-(default, 'Acondicionador Kerastase', 'Acondicionador Kerastase super hidratante', 4),
-(default, 'Desmaquillante Lancome', 'Desmaquillante Lancome super suave', 5),
-(default, 'Contorno Rare Beauty', 'Contorno Rare Beauty en crema', 1),
-(default, 'Rubor Mac', 'Rubor Mac super pigmentado', 2),
-(default, 'Sombra Dior', 'Paleta de sombras colores neutros', 3),
-(default, 'Gloss Dior', 'Gloss super brillante', 4),
-(default, 'Set Labial Kylie Cosmetics', 'Set de labios color nude', 5);
+insert into productos (id, nombre, descripcion, usuarioId, cover)
+values (default, 'Shampoo Kerastase', 'Shampoo Kerastase super hidratante', 1,'/images/products/shampoo.jpg'),
+(default, 'Labial Dior', 'Labial Dior rojo humectante', 2, '/images/products/labialdior.webp'),
+(default, 'Mascara Lancome', 'Máscara Lancome grandiosa', 3, '/images/products/mascaralancome.jpg'),
+(default, 'Acondicionador Kerastase', 'Acondicionador Kerastase super hidratante', 4, '/images/products/acondicionador.jpg'),
+(default, 'Desmaquillante Lancome', 'Desmaquillante Lancome super suave', 5, '/images/products/desmaquillante.webp'),
+(default, 'Contorno Rare Beauty', 'Contorno Rare Beauty en crema', 1, '/images/products/contornorare.jpeg'),
+(default, 'Rubor Mac', 'Rubor Mac super pigmentado', 2, '/images/products/rubormac.avif'),
+(default, 'Sombra Dior', 'Paleta de sombras colores neutros', 3, '/images/products/sombradior.jpeg'),
+(default, 'Gloss Dior', 'Gloss super brillante', 4, '/images/products/glossdior.jpeg'),
+(default, 'Set Labial Kylie Cosmetics', 'Set de labios color nude', 5, '/images/products/labialkylie.jpeg');
 
 insert into comentarios (id, comentarios, usuarioId, productoId)
 values (default, '¿Tienen stock disponible?', 1, 1),
@@ -102,4 +103,3 @@ values (default, '¿Tienen stock disponible?', 1, 1),
 (default, '¿Tienen envío gratis?', 3, 10),
 (default, '¿Tienen cuotas sin interes?', 4, 10),
 (default, '¿El producto es original?', 5, 10);
-
